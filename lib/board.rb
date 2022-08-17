260a4efb6a0f706e0fe789beccd59dacaa3ae9a8
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-08-17 01:14:18 -0500

    initializes class and adds position method

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,28 @@
-class Board; end
+class Board
+  attr_accessor :board
+
+  def initialize
+    self.board = [
+      [1, ' |', 2, ' |', 3],
+      ['--', '|', '--', '|', '--'],
+      [4, ' |', 5, ' |', 6],
+      ['--', '|', '--', '|', '--'],
+      [7, ' |', 8, ' |', 9]
+    ]
+  end
+
+  def position(number)
+    position_index = nil
+
+    board.each do |row|
+      position_index = row.index(number)
+      return position_index unless position_index.nil?
+    end
+  end
+end
+
+b = Board.new
+
+# b.board.each { |i| puts i.join }
+
+puts b.position(1)
