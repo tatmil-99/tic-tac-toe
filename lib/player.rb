ef9fba1bc42a79d2e8b87e4bda590858d577ee32
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-08-18 21:57:46 -0500

    allows player to choose mark to use for game and determines if mark has been used already

--- lib/player.rb
+++ lib/player.rb
@@ -1,1 +1,38 @@
-class Player; end
+class Player
+  @@x_used = false
+  @@o_used = false
+
+  attr_reader :mark
+
+  def initialize(mark)
+    self.mark = mark
+  end
+
+  def mark=(mark)
+    if mark == 'x' && x_used? == false
+      @mark = mark
+      self.x_used = true
+    elsif mark == 'o' && o_used? == false
+      @mark = mark
+      self.o_used = true
+    else
+      puts 'mark has been taken'
+    end
+  end
+
+  def x_used?
+    @@x_used
+  end
+
+  def x_used=(bool)
+    @@x_used = bool
+  end
+
+  def o_used?
+    @@o_used
+  end
+
+  def o_used=(bool)
+    @@o_used = bool
+  end
+end
