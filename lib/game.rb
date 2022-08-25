3a0e40a366ab58ef20b99fa14a14698794e869fe
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2022-08-25 01:07:18 -0500

    creates start method and calls required objects for beginning of game

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,17 @@
-class Game; end
+require_relative 'player'
+# require_relative 'player_input'
+
+class Game
+  # future game loop here
+  def start
+    puts 'Player 1:'
+    player1_input = PlayerInput.new
+    player1_input.query_mark
+    player1 = Player.new(player1_input.mark)
+
+    puts 'Player 2:'
+    player2_input = PlayerInput.new
+    player2_input.query_mark
+    player2 = Player.new(player2_input.mark)
+  end
+end
